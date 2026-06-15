09ec9bb3b40cf181f32698071ca7aeffc7120e4b
chandrasekhar-alti <althichandu123@gmail.com>
2026-06-15 22:52:09 +0530

    first commit

---
+++ src/main/java/com/example/stylestackapp/StyleStackAppApplication.java
@@ -0,0 +1,13 @@
+package com.example.stylestackapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StyleStackAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StyleStackAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stylestackapp/StyleStackAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stylestackapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StyleStackAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
